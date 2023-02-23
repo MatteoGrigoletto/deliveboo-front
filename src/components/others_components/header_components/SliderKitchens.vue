c5fd2d214509d1c964373cc0b0a50677c0b61113
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-02-23 16:11:46 +0100

    modica card category e media query

--- src/components/others_components/header_components/SliderKitchens.vue
+++ src/components/others_components/header_components/SliderKitchens.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="box">
-    <div class="kitchen" v-for="item in findKitchensRestaurants(store.restaurants)">
-        {{ item.name }}
+<div class="container-kitchens">
+    <div class="kitchen" v-for="item in findKitchensRestaurants(store.restaurants)"> 
         <img :src="store.restaurants[0].image " alt="">
+        <span>{{ item.name }}</span>
         <!-- <router-link :to="{name: 'RestaurantCategory',params: { category: item.name }}">Prodotti</router-link> -->
     </div>
 </div>
@@ -28,26 +28,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.box{
+.container-kitchens{
     padding: 20px 0px;
     display: flex;
     justify-content:space-around;
     .kitchen{
         width: 10%;
         height: 100px;
-        background-color: blanchedalmond;
-        
+        position: relative;
+    }
+    span{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        color: white;
+        font-weight: bold ;
     }
 
 }
 
-@media screen and (max-width:800px){
-.box{
-    flex-wrap: wrap;
+@media screen and (max-width:600px){
+    .container-kitchens{
+        display: none;
+  }
+}
+@media screen and (min-width:601px) and (max-width:1000px){
+    .container-kitchens{
+        flex-wrap: wrap;
     .kitchen{
         margin: 5px;
         width: calc(100% / 5 - 10px);
     }
-}
+  }
 }
 </style>
