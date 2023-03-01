8a8dd3b61c96aedd052a51caaca75efbf0a40524
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-03-01 14:33:40 +0100

    Aggiunge componente al footer delle info

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,41 +4,7 @@
      <PartnerSection/>
     </section>
     <section class="section-site">
-      <div class="info-site">
-        <div class="info-site__links">
-          <h5>Scopri Deliveboo</h5>
-          <ul>
-            <li class="info-site__link"><a href="#">Investitori</a></li>
-            <li class="info-site__link"><a href="#">Ristoranti</a></li>
-            <li class="info-site__link"><a href="#">Pressroom</a></li>
-            <li class="info-site__link"><a href="#">Programmazione</a></li>
-            <li class="info-site__link"><a href="#">Design</a></li>
-          </ul>
-        </div>
-        <div class="info-site__legal">
-          <h5>Note legali</h5>
-          <ul>
-            <li class="info-site__link"><a href="#">Termini & Condizioni</a></li>
-            <li class="info-site__link"><a href="#">Informativa sulla privacy</a></li>
-            <li class="info-site__link"><a href="#">Cookies</a></li>
-          </ul>
-        </div>
-        <div class="info-site__help">
-          <h5>Aiuto</h5>
-          <ul>
-            <li class="info-site__link"><a href="#">FAQ</a></li>
-            <li class="info-site__link"><a href="#">Contatti</a></li>
-            <li class="info-site__link"><a href="#">Tipi di cucina</a></li>
-          </ul>
-        </div>
-        <div class="info-site__app">
-          <h5>Porta Deliveboo con te</h5>
-          <ul>
-            <li class="info-site__link"><a href="#">Investitori</a></li>
-            <li class="info-site__link"><a href="#">Chi siamo</a></li>
-          </ul>
-        </div>
-      </div>
+      <InfoSection/>
     </section>
     <section class="section-social">
       <div class="info-social">
@@ -56,6 +22,7 @@
 
 <script>
 import PartnerSection from './others_components/footer_components/PartnerSection.vue';
+import InfoSection from './others_components/footer_components/InfoSection.vue';
 export default {
   name: "AppFooter",
   data(){
@@ -65,6 +32,7 @@ export default {
   },
   components: {
     PartnerSection,
+    InfoSection,
   }
 };
 </script>
@@ -82,71 +50,28 @@ footer {
       display: flex;
     } 
   }
-  
-  
-  // Info Partner
   .section-partner{
     background-color: rgb(94, 92, 92);
-    
   }
-  // Links
   
-  .section-site{
-    .info-site {
-      margin-top: 100px;
-      justify-content: center;
-      
-      > div {
-        width: calc(100% / 4 - 6px);
-        margin: 3px;
-        background-color: black;
-        padding: 20px;
+  .section-social{
+    .info-social__app{
+      width: 60%;  
+      ul{
+        display: flex;
+        justify-content: flex-start;
         
-        ul {
-          padding: 0px;
-          
-          li {
-            margin: 25px 0px;
-          }
+        li{
+          margin:10px 20px 10px 20px ;         
         }
       }
     }
   }
 }
-// Social
-
-.section-social{
-  .info-social__app{
-    
-    ul{
-      display: flex;
-      justify-content: flex-start;
-      
-      li{
-        margin:10px 20px 10px 20px ;         
-      }
-    }
-  }
-}
-
+  
 //  media
 
 @media screen and (max-width: 600px) {
-
-
-
-    div{
-       width: 100% !important;
-      flex-wrap: wrap;
-      .info-partner__single{
-        width: 100% !important;
-        
-        &__info{
-          font-size: 0.7rem;
-          width: 100% !important;
-        }
-      }
-    }
   }
 
 @media screen and (min-width: 601px) and (max-width: 960px) {
