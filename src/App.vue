3c3556c0b2f5564cc5face75e27f4550c210fc1e
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-03-03 16:32:32 +0100

    Add custom scroll bar

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,23 @@ export default {
 
 <style lang="scss">
 @import "./style/global.scss";
+/* Seleziona la scrollbar */
+::-webkit-scrollbar {
+  width: 10px;
+  height: 4px;
+  border-radius: 3px;
+  background-color: black;
+}
 
+/* Seleziona la parte colorata della scrollbar */
+::-webkit-scrollbar-thumb {
+  background-color: #8c8b8b;
+  border-radius: 3px;
+}
+
+/* Seleziona la parte grigia della scrollbar */
+::-webkit-scrollbar-track {
+  background-color: transparent;
+  border-radius: 3px;
+}
 </style>
